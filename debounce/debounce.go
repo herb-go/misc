9e38026bba1d437d1de12de90f7c2c270f1950b1
Jarlyyn <jarlyyn@qq.com>
2019-01-23 21:48:57 +0800

    update

--- debounce/debounce.go
+++ debounce/debounce.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+//DefaultMaxDurationMagnification efault max duration magnification
+var DefaultMaxDurationMagnification int64 = 2
+
 //Debounce debounce struct
 type Debounce struct {
 	//Duration debounce duration
@@ -25,47 +28,43 @@ type Debounce struct {
 func (d *Debounce) Exec() bool {
 	d.lock.Lock()
 	defer d.lock.Unlock()
-	if d.timer == nil {
-		d.timer = time.NewTimer(d.Duration)
-		d.deadline = time.Now().Add(d.MaxDuration)
-		go func() {
-			if d.Leading {
-				d.Callback()
-			}
-			<-d.timer.C
-			d.timer = nil
-			if !d.Leading {
-				d.Callback()
-			}
-		}()
-		if d.Leading {
-			return true
+	var success = false
+	if d.timer != nil {
+
+		now := time.Now()
+		duration := d.deadline.Sub(now)
+		if duration > d.Duration {
+			duration = d.Duration
 		}
+		success = d.timer.Reset(duration)
+	}
+	if success {
 		return false
 	}
-	now := time.Now()
-	duration := d.deadline.Sub(now)
-	if duration > d.Duration {
-		duration = d.Duration
-	}
-	success := d.timer.Reset(duration)
-	if success == false {
-		go func() {
-			d.Exec()
-		}()
+	d.timer = time.NewTimer(d.Duration)
+	d.deadline = time.Now().Add(d.MaxDuration)
+	go func() {
 		if d.Leading {
-			return true
+			d.Callback()
 		}
-		return false
+		<-d.timer.C
+		d.timer = nil
+		if !d.Leading {
+			d.Callback()
+		}
+	}()
+	if d.Leading {
+		return true
 	}
 	return false
+
 }
 
 //New create new debounce with given duration and callback
 func New(duration time.Duration, callback func()) *Debounce {
 	d := &Debounce{}
 	d.Duration = duration
-	d.MaxDuration = 2 * duration
+	d.MaxDuration = time.Duration(DefaultMaxDurationMagnification) * duration
 	d.Callback = callback
 	return d
 }
