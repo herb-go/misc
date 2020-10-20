59171d0f9703546070fe4151789f8162af39a866
jarlyyn <jarlyyn@qq.com>
2020-10-21 00:57:49 +0800

    update

--- binarystream/errors.go
+++
@@ -1,6 +0,0 @@
-package binarystream
-
-import "errors"
-
-var ErrDataLengthNotMatch = errors.New("binarystream data length not match")
-var ErrDataTypeNotSupported = errors.New("binarystream data type not supported")
